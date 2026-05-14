7126597ca8f85e2ce650ad6c89ce2335989b5f7b
Santosh190512 <santoshbehera34514@gmail.com>
2026-05-14 12:39:37 +0530

    rcb

--- backend/core/resume_analyzer/views.py
+++ backend/core/resume_analyzer/views.py
@@ -19,7 +19,7 @@ from .utils import extract_text
 if settings.GEMINI_API_KEY:
     genai.configure(api_key=settings.GEMINI_API_KEY)
 
-model = genai.GenerativeModel("gemini-2.5-flash")
+model = genai.GenerativeModel("google/gemini-2.5-flash")
 
 
 def extract_score(analysis):
