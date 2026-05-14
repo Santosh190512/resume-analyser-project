1606d7e1b7c8f33851422f41ec706ff1933119bd
Santosh190512 <santoshbehera34514@gmail.com>
2026-05-14 12:25:58 +0530

    model change

--- backend/core/resume_analyzer/views.py
+++ backend/core/resume_analyzer/views.py
@@ -19,7 +19,7 @@ from .utils import extract_text
 if settings.GEMINI_API_KEY:
     genai.configure(api_key=settings.GEMINI_API_KEY)
 
-model = genai.GenerativeModel("Gemini 2 Flash Lite")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 
 def extract_score(analysis):
